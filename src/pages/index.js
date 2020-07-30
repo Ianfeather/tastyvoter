0bf79c20eeca54792808b2eed92b7b89b55ead3a
Ian Feather <info@ianfeather.co.uk>
2020-07-30 10:55:53 +0100

    pointless animations part 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const VOTING_TIMER_DURATION = 20;
 const Index = () => {
   let [recipes, setRecipes] = useState([]);
   let [eliminatedRecipes, setEliminatedRecipes] = useState([]);
-  let [game, setGame] = useState(null);
+  let [game, setGame] = useState({ id:1, complete: true, winner: {"id":"7fbaa316-9084-43f0-be01-4d90854562aa","name":"Chicken Tikka Biryani","imageUrl":"https://img.buzzfeed.com/video-api-prod/assets/e0852050640d4474aaf3542d61f2568a/FB.jpg","canonicalUrl":"https://tasty.co/recipe/chicken-tikka-biryani","description":"","votes":11,"games":{"items":[{"id":"d2eb8f13-4f18-490c-830a-6b45a9cae9a2","createdAt":"2020-07-29T23:34:32.088Z","updatedAt":"2020-07-29T23:34:32.088Z"},{"id":"b6a1617b-f3a7-49f5-9c17-1081f043c654","createdAt":"2020-07-29T23:34:31.999Z","updatedAt":"2020-07-29T23:34:31.999Z"},{"id":"3dfbd118-c2f3-4622-9161-26afe31654de","createdAt":"2020-07-29T23:34:31.827Z","updatedAt":"2020-07-29T23:34:31.827Z"},{"id":"d1904084-e7c0-4df7-ad9d-e515ad522fd7","createdAt":"2020-07-29T22:05:07.907Z","updatedAt":"2020-07-29T22:05:07.907Z"},{"id":"19417351-ff76-413e-ad83-1419fa43becc","createdAt":"2020-07-29T23:34:32.695Z","updatedAt":"2020-07-29T23:34:32.695Z"},{"id":"39da8460-82ab-47aa-bb89-69fc56598b98","createdAt":"2020-07-29T22:02:17.163Z","updatedAt":"2020-07-29T22:02:17.163Z"},{"id":"89e19706-384f-4b38-ac18-c286787ab5e9","createdAt":"2020-07-29T23:28:30.229Z","updatedAt":"2020-07-29T23:28:30.229Z"},{"id":"62387132-8db1-44b7-abf9-06f8b11071fd","createdAt":"2020-07-29T21:59:17.510Z","updatedAt":"2020-07-29T21:59:17.510Z"},{"id":"cb9269d9-c9b3-491c-a3a2-2e2ea5934b95","createdAt":"2020-07-29T23:34:32.382Z","updatedAt":"2020-07-29T23:34:32.382Z"},{"id":"cbf0ca0e-ff18-4a3e-830b-8b416251629c","createdAt":"2020-07-29T23:28:29.688Z","updatedAt":"2020-07-29T23:28:29.688Z"},{"id":"61a1cef9-adb3-4cd0-b269-bff52b885d73","createdAt":"2020-07-29T22:32:22.713Z","updatedAt":"2020-07-29T22:32:22.713Z"},{"id":"958ae91a-4f93-45aa-bd47-f41db4cd638c","createdAt":"2020-07-29T22:08:56.762Z","updatedAt":"2020-07-29T22:08:56.762Z"},{"id":"d2ebe843-82bf-4d0d-9971-aafc0d2e0c22","createdAt":"2020-07-29T23:34:32.232Z","updatedAt":"2020-07-29T23:34:32.232Z"},{"id":"8a7dc3aa-b70c-4b62-9506-2ace92c80592","createdAt":"2020-07-29T23:28:29.247Z","updatedAt":"2020-07-29T23:28:29.247Z"},{"id":"20903ff2-80ff-48a0-a6a0-5a40d96b9893","createdAt":"2020-07-29T23:28:30.174Z","updatedAt":"2020-07-29T23:28:30.174Z"},{"id":"f5383ee4-3a25-4456-a871-ec60fec58d4d","createdAt":"2020-07-29T22:09:59.979Z","updatedAt":"2020-07-29T22:09:59.979Z"},{"id":"55fec767-559b-49ec-9bfd-1442a868fd7b","createdAt":"2020-07-29T23:28:29.770Z","updatedAt":"2020-07-29T23:28:29.770Z"},{"id":"fd2d684c-eff9-4f57-af76-13718393ab0c","createdAt":"2020-07-29T22:04:19.519Z","updatedAt":"2020-07-29T22:04:19.519Z"},{"id":"e4ff04c3-834f-447e-988d-abe1ae1717ff","createdAt":"2020-07-29T22:00:59.544Z","updatedAt":"2020-07-29T22:00:59.544Z"}],"nextToken":null},"createdAt":"2020-07-28T09:34:08.859Z","updatedAt":"2020-07-30T08:35:20.318Z"}});
 
   let [isAdmin, setIsAdmin] = useState(false);
   let [introTimer, setIntroTimer] = useState(INTRO_TIMER_DURATION);
@@ -23,6 +23,7 @@ const Index = () => {
   /*
    * Functions called from useEffect hooks
    */
+
 
   async function getInitialState() {
     const { data } = await API.graphql(graphqlOperation(customQueries.initialState));
@@ -192,11 +193,18 @@ const Index = () => {
    */
   const totalVotes = recipes.reduce((acc, next) => acc + next.votes, 0);
 
+  // const state = {
+  //   pregame: !game,
+  //   intro: game && introTimer > 0,
+  //   voting: game && introTimer === 0 && votingTimer > 0,
+  //   complete: game && introTimer === 0 && votingTimer === 0
+  // };
+
   const state = {
-    pregame: !game,
-    intro: game && introTimer > 0,
-    voting: game && introTimer === 0 && votingTimer > 0,
-    complete: game && introTimer === 0 && votingTimer === 0
+    pregame: false,
+    intro: false,
+    voting: true,
+    complete: false
   };
 
   return (
@@ -204,9 +212,15 @@ const Index = () => {
       <div className={styles.headerContainer}>
         {
           state.voting && (
-            <div className={styles.votingTimer}>
-              {votingTimer}
-            </div>
+            <>
+              <div className={styles.explainer}>
+                <p className={styles.title}>What are we cooking?! Cast your vote.</p>
+                <p className={styles.subtitle}>Ingredients for the winning recipe will be 50% off at Walmart!</p>
+              </div>
+              <div className={styles.votingTimer}>
+                {votingTimer}
+              </div>
+            </>
           )
         }
       </div>
@@ -227,11 +241,17 @@ const Index = () => {
         )
       }
       {
-        state.voting && recipes.map(({ name, id, canonicalUrl, imageUrl, votes }) => {
+        state.voting && recipes.map(({ name, id, canonicalUrl, imageUrl, votes }, index) => {
           const percentOfVote = totalVotes > 0 ? (votes / totalVotes) * 100 : 0;
           const disabled = eliminatedRecipes.some(_id => _id === id);
+          const positionClassName = styles[`position${index}`];
+          let completeClassName = '';
+          if (game && game.complete) {
+            completeClassName = game.winner.id === id ? styles.winner : styles.loser;
+          }
+          game && game.winner.id === id;
           return (
-            <div className={`${styles.recipe} ${disabled ? styles.disabled : ''}`} key={id}>
+            <div className={`${styles.recipe} ${disabled ? styles.disabled : ''} ${completeClassName} ${positionClassName}`} key={id}>
               <img className={styles.image} src={imageUrl} alt={`Image of ${name}`}/>
               <h3 className={styles.name}>
                 <a href={canonicalUrl}>{name}</a>
@@ -251,21 +271,16 @@ const Index = () => {
       {
         state.complete && game.winner && (
           <div className={styles.winningContainer}>
+            And the winner is...
             <div className={styles.winningRecipe}>
-              <div className={styles.name}>
-                And the winner is...
+              <img className={styles.winningImage} src={game.winner.imageUrl} alt={`Image of ${game.winner.name}`}/>
+              <div className={`${styles.winningName} ${styles.name}`}>
                 <h3><a href={game.winner.canonicalUrl}>{game.winner.name}!</a></h3>
               </div>
-              <img className={styles.winningImage} src={game.winner.imageUrl} alt={`Image of ${game.winner.name}`}/>
-            </div>
-            <div className={styles.rightColumn}>
               <div className={styles.walmartMoneyPlease}>
                 <h3 className={styles.walmartTitle}>Get the winning ingredients 50% off thanks to our dear friends at walmart!</h3>
                 <button className={styles.walmartButton}>Buy now!</button>
                 <div className={styles.walmartDisclaimer}>(Walmart please, if you&apos;re listening, it&apos;s a great idea)</div>
-              </div>
-              <div className={styles.video}>
-                <img className={styles.videoImage} src="https://user-images.githubusercontent.com/814861/88680590-93453d80-d0e8-11ea-862d-70e3730f5f3a.png" alt="video still of cooking the recipe"/>
               </div>
             </div>
           </div>
